62948d28f1abed3bb8a2711c3f5be42d15b54b0c
priyansh-ankre <bod123priyansh@gmail.com>
2020-08-31 21:59:34 +0530

    [Priyansh]Added:DriverController in ParkingLotApi

---
+++ ParkingLotApi/Controllers/DriverController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingLotBussinessLayer;
+using ParkingLotModelLayer;
+
+namespace ParkingLotApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        private readonly IParkingLotBussiness parkingLotBussiness;
+        public DriverController(IParkingLotBussiness parkingLotBussiness)
+        {
+            this.parkingLotBussiness = parkingLotBussiness;
+        }
+
+        [HttpPost]
+        public IActionResult AddParking(Parking parking)
+        {
+
+            var parkingResult = this.parkingLotBussiness.AddParkingData(parking);
+            if (parkingResult != null)
+            {
+                return this.Ok(parkingResult);
+            }
+            return this.BadRequest();
+        }
+
+        [HttpPost]
+        [Route("Unparking")]
+        public IActionResult Unparking(int parkingSlotId)
+        {
+            var unparkingResult = this.parkingLotBussiness.Unparking(parkingSlotId);
+            if (unparkingResult != null)
+            {
+                return this.Ok(unparkingResult);
+            }
+            return this.BadRequest();
+        }
+    }
+}
