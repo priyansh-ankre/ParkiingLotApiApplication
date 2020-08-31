4b817291ab5b180819c8d1f83f7b6ad6094a5627
priyansh-ankre <bod123priyansh@gmail.com>
2020-08-31 22:09:26 +0530

    [Priyansh]Added:PoliceController in ParkingLotApi

---
+++ ParkingLotApi/Controllers/PoliceController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingLotBussinessLayer;
+using ParkingLotModelLayer;
+
+namespace ParkingLotApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PoliceController : ControllerBase
+    {
+        private readonly IParkingLotBussiness parkingLotBussiness;
+        public PoliceController(IParkingLotBussiness parkingLotBussiness)
+        {
+            this.parkingLotBussiness = parkingLotBussiness;
+        }
+
+        [HttpPost]
+        public IActionResult AddParking(Parking parking)
+        {
+
+            var parkingResult = this.parkingLotBussiness.AddParkingData(parking);
+            if (parkingResult != null)
+            {
+                return this.Ok(parkingResult);
+            }
+            return this.BadRequest();
+        }
+
+        [HttpPost]
+        [Route("Unparking")]
+        public IActionResult Unparking(int parkingSlotId)
+        {
+            var unparkingResult = this.parkingLotBussiness.Unparking(parkingSlotId);
+            if (unparkingResult != null)
+            {
+                return this.Ok(unparkingResult);
+            }
+            return this.BadRequest();
+        }
+
+        [HttpPost]
+        [Route("GetParkingByVehicleNumber")]
+        public IActionResult GetParkingDataByVehicleNumber(string vehicleNumber)
+        {
+            var getResult = this.parkingLotBussiness.GetParkingDataByVehicleNumber(vehicleNumber);
+            if (getResult != null)
+            {
+                return this.Ok(getResult);
+            }
+            return this.BadRequest();
+        }
+
+        [HttpPost]
+        [Route("GetParkingByParkingSlot")]
+        public IActionResult GetParkingDataByParkingSlot(int parkingSlot)
+        {
+            var getResult = this.parkingLotBussiness.GetParkingDataByParkingSlot(parkingSlot);
+            if (getResult != null)
+            {
+                return this.Ok(getResult);
+            }
+            return this.BadRequest();
+        }
+    }
+}
